32e0bd78fb67dc6e289e435a6500a9efb640ecfb
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-07 15:33:29 -0600

    dont record cursor

--- home/record.sh
+++ home/record.sh
@@ -2,5 +2,5 @@
 if pgrep -f "gpu-screen-recorder" >/dev/null; then
     pkill -SIGINT -f "gpu-screen-recorder"
 else
-    "gpu-screen-recorder" -w DP-2 -f 60 -a default_output -o "$HOME/videos/$(date +"%d-%m-%Y_%H-%M-%S").mp4"
+    "gpu-screen-recorder" -w DP-2 -f 60 -cursor no -a default_output -o "$HOME/videos/$(date +"%d-%m-%Y_%H-%M-%S").mp4"
 fi
