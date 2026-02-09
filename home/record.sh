61f6702d5bf9b7521ff97fbaca60c94fba3d6e92
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-09 12:41:30 -0600

    recording finished indicator

--- home/record.sh
+++ home/record.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if pgrep -f "gpu-screen-recorder" >/dev/null; then
     pkill -SIGINT -f "gpu-screen-recorder"
+    notify-send "gpu-screen-recorder" "Recording finished"
 else
     "gpu-screen-recorder" -w DP-2 -f 60 -cursor no -a default_output -o "$HOME/videos/$(date +"%d-%m-%Y_%H-%M-%S").mp4"
 fi
