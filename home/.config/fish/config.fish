71d880a632a00d480e01891f6cdbe59ab44ec84b
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-11 18:47:23 -0600

    fish

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,7 @@
+export PATH="$PATH:/usr/share/dotnet"
+export DOTNET_ROOT="/usr/share/dotnet/"
+export PATH="$PATH:/home/envoidia/.dotnet/tools"
+
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
