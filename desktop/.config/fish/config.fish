b72216cba39561c014cbdaf5b2bc807ab6e8a820
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-20 12:39:22 -0600

    lsd

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -2,6 +2,8 @@ export PATH="$PATH:/usr/share/dotnet"
 export DOTNET_ROOT="/usr/share/dotnet/"
 export PATH="$PATH:/home/envoidia/.dotnet/tools"
 
+alias ls=lsd
+
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
