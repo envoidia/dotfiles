13a34cb405eada2cfc86a8d711773d383ab562ef
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-19 23:44:21 -0600

    add laptop files

--- laptop/record.sh
+++ laptop/record.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+
 if pgrep -f "gpu-screen-recorder" >/dev/null; then
     pkill -SIGINT -f "gpu-screen-recorder"
     notify-send "gpu-screen-recorder" "Recording finished"
 else
-    "gpu-screen-recorder" -w DP-2 -f 60 -cursor no -a default_output -o "$HOME/videos/$(date +"%d-%m-%Y_%H-%M-%S").mp4"
+    "gpu-screen-recorder" -w eDP-1 -f 60 -cursor no -a defaultoutput -o "$HOME/videos/$(date +"%d-%m-%Y%H-%M-%S").mp4 -fallback-cpu-encoding yes"
 fi
