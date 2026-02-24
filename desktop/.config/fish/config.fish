9d564094b988a81e07aec39c794f42e6e3d40ddf
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-24 17:38:27 -0600

    fd/rg always include hidden files

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -2,6 +2,8 @@ alias ls="lsd -A"
 alias lst="lsd -A --tree"
 alias cbp=wl-paste
 alias drd=dragon-drop
+alias fd="fd -H"
+alias rg="rg --hidden"
 
 set DOTNET_ROOT "/usr/share/dotnet/"
 set PATH "$PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools"
