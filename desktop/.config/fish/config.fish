e1492a3db9ae5525bbd593b143352886b57509fb
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-26 15:18:01 -0600

    du alias, fd/rg ignore case

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -2,8 +2,9 @@ alias ls="lsd -A"
 alias lst="lsd -A --tree"
 alias cbp=wl-paste
 alias drd=dragon-drop
-alias fd="fd -H"
-alias rg="rg --hidden"
+alias fd="fd -iH"
+alias rg="rg -i --hidden"
+alias du="du -h"
 
 set DOTNET_ROOT "/usr/share/dotnet/"
 set PATH "$PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools"
