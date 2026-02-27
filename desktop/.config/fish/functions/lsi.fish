8adb3fc26d501fa551176faca5edf712545d3b16
envoidia <76633436+envoidia@users.noreply.github.com>
2026-02-27 01:44:17 -0600

    better lsi

--- desktop/.config/fish/functions/lsi.fish
+++ desktop/.config/fish/functions/lsi.fish
@@ -1,5 +1,42 @@
 function lsi -d "Show a grid of image previews"
-    set -l grid 8
-    test -n "$argv[1]" && set grid $argv[1]
-    timg --upscale --grid=$grid --frames=1 --title *
+    argparse 'h/help' 'c/columns=' 'R/recursive' -- $argv
+    or return
+
+    if set -q _flag_h
+        echo -e "usage: lsi [options] <file/dir> [<file/dir>...]\nOptions:\n    -h, --help: Print this help message and exit\n    -c, --columns <amount>: Set grid column count (default 8)\n    -R, --recursive: Recurse into directories"
+        return
+    end
+
+    if not set -q _flag_c
+        set _flag_c 8
+    end
+        
+    set -l files
+
+    set -l glob
+
+    if set -q _flag_R
+        set glob {,**/}*
+    else
+        set glob *
+    end
+
+    if test (count $argv) -eq 0
+        for f in $glob
+            test -f "$f" && set -a files "$f"
+        end
+    else
+        for arg in $argv
+            if test -d "$arg"
+                for f in "$arg"/$glob
+                    test -f "$f" && set -a files "$f"
+                end
+            else
+                set -a files "$arg"
+            end
+        end
+
+    end
+    
+    timg --upscale --grid=$_flag_c --frames=1 --title $files
 end
