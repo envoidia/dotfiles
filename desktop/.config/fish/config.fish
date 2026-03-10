96c7503347ed63a53c085da56608865d567021d6
envoidia <76633436+envoidia@users.noreply.github.com>
2026-03-10 02:45:33 -0500

    cbc file completions

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -10,5 +10,9 @@ alias drd="dragon-drop --and-exit"
 
 alias mv="mv -i"
 
+# Enable file path completion
+# This is technically wrong for wl-copy, but I only use it via cbc, and adding it to cbc doesn't work
+complete -c wl-copy -F
+
 set DOTNET_ROOT "/usr/share/dotnet/"
 set PATH "$PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools"
--- desktop/.config/fish/functions/cbc.fish
+++ desktop/.config/fish/functions/cbc.fish
@@ -9,7 +9,7 @@ function cbc -w "wl-copy" -d "Copies text and files"
         if test -f "$input"
             set mime (xdg-mime query filetype "$input" 2>/dev/null)
 
-            # Pretend JPEGs are PNGs because I can't paste JPEGs otherwise (wl-clipboard bug?)
+            # Pretend JPEGs are PNGs because I can't paste JPEGs into some programs otherwise (wl-clipboard bug?)
             if test "$mime" = "image/jpeg"
                 wl-copy --type image/png < "$input"
             else
