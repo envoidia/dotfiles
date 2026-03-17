426cfc0c2b794c4a7525d68dcb7aa59359ac8881
envoidia <76633436+envoidia@users.noreply.github.com>
2026-03-17 16:44:02 -0500

    remove unneccessary script

--- desktop/.config/waybar/gpu_usage.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-set -l util $(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
-set -l vram $(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits 2>/dev/null | awk '{printf "%.2f", $1/1024}')
-
-echo $util% $vram"GB"
