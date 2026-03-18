6f7c6e17762eb6df8a1e20ee7b3441c2feb38c45
envoidia <76633436+envoidia@users.noreply.github.com>
2026-03-18 00:50:40 -0500

    optimize menu item order

--- desktop/.config/logout_menu.fish
+++ desktop/.config/logout_menu.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
-switch $(echo -e "logout\nshutdown\nreboot" | fsel --dmenu)
-    case logout
-        command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
+switch $(echo -e "shutdown\nreboot\nlogout" | fsel --dmenu)
     case shutdown
         shutdown now
     case reboot
         reboot
+    case logout
+        command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
 end
