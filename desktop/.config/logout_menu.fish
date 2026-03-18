6d8d22e6e35f702f6b30a10014060e7666a51d7a
envoidia <76633436+envoidia@users.noreply.github.com>
2026-03-18 00:39:08 -0500

    remove unneeded cancel opt

--- desktop/.config/logout_menu.fish
+++ desktop/.config/logout_menu.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-switch $(echo -e "cancel\nlogout\nshutdown\nreboot" | fsel --dmenu)
+switch $(echo -e "logout\nshutdown\nreboot" | fsel --dmenu)
     case logout
         command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
     case shutdown
