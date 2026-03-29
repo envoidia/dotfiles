2ea17fe7cf83de395bc8c86d7b4ae163c950ed72
envoidia <76633436+envoidia@users.noreply.github.com>
2026-03-29 15:45:30 -0500

    remove unneeded quotes

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -3,7 +3,8 @@ alias ll="lsd -AlS"
 alias lt="lsd -A --tree"
 alias fd="fd -iH"
 alias rg="kitten hyperlinked-grep -i --hidden"
-alias du="dust"
+alias du=dust
+alias cat=bat
 
 alias cbp=wl-paste
 alias drd="dragon-drop --and-exit"
@@ -19,5 +20,5 @@ set fzf_fd_opts -iH
 fzf_configure_bindings --directory=ctrl-super-f --git_log=ctrl-super-l --git_status=ctrl-super-s \
     --history=ctrl-super-h --processes=ctrl-super-p --variables=ctrl-super-v
 
-set DOTNET_ROOT "/usr/share/dotnet/"
-set PATH "$PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools"
+set DOTNET_ROOT /usr/share/dotnet
+set PATH $PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools
--- desktop/.config/fish/functions/cbc.fish
+++ desktop/.config/fish/functions/cbc.fish
@@ -1,19 +1,19 @@
 # Convenience wrapper for wl-copy
-function cbc -w "wl-copy" -d "Copies text and files"
+function cbc -w wl-copy -d "Copies text and files"
     if test $(count $argv) -eq 0
         wl-copy
     else
         set input $argv[1]
 
         # Copy file if input is a filepath
-        if test -f "$input"
-            set mime $(xdg-mime query filetype "$input" 2>/dev/null)
+        if test -f $input
+            set mime $(xdg-mime query filetype $input 2>/dev/null)
 
             # Pretend JPEGs are PNGs because I can't paste JPEGs into some programs otherwise (wl-clipboard bug?)
-            if test "$mime" = "image/jpeg"
-                wl-copy --type image/png < "$input"
+            if test $mime = image/jpeg
+                wl-copy --type image/png < $input
             else
-                wl-copy < "$input"
+                wl-copy < $input
             end
         # Copy text if input is text
         else
