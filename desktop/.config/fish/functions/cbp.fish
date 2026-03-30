f2a7d78d64cdd9206dc5e22177a5dfa1ae0fe4d6
envoidia <76633436+envoidia@users.noreply.github.com>
2026-03-29 19:10:24 -0500

    fix binary pasting

--- desktop/.config/fish/functions/cbp.fish
+++ desktop/.config/fish/functions/cbp.fish
@@ -2,7 +2,7 @@
 function cbp -w wl-paste -d "Pastes text and files"
     # Resolve URI
     set -l content $(wl-paste 2>/dev/null)
-    if string match -q 'file://*' $content
+    if string match -q 'file://*' "$content"
         wl-copy < $(string replace --regex '^file://' '/' $content)
     end
 
