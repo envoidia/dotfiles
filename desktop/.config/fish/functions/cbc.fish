a937a8f8b421b2c6312d699ad4d96096ccd5cef7
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-01 18:34:24 -0500

    copy audio as URI

--- desktop/.config/fish/functions/cbc.fish
+++ desktop/.config/fish/functions/cbc.fish
@@ -8,7 +8,7 @@ function cbc -w wl-copy -d "Copies text and files"
     argparse 'h/help' 'l/link' 'n/nolink' -- $argv
 
     if set -q _flag_h
-        echo -e "usage: cbc [options] <file/dir>\nOptions:\n    -h, --help: Print this help message and exit\n    -l, --link: Copy file as link/URI\n    -n, --nolink: Never copy file as link. Without this option, videos and GIFs will copy as links"
+        echo -e "usage: cbc [options] <file/dir>\nOptions:\n    -h, --help: Print this help message and exit\n    -l, --link: Copy file as link/URI\n    -n, --nolink: Never copy file as link. Without this option, audio, videos, and GIFs will copy as links"
         return
     end
     
@@ -17,11 +17,13 @@ function cbc -w wl-copy -d "Copies text and files"
     # Copy file
     if test -f $input
         set -l mime $(xdg-mime query filetype $input 2>/dev/null)
+        set -l mime_prefix $(string split -f 1 / $mime)
 
         # Copy as URI
         if not set -q _flag_n
             # Separate if statement due to operator precedence
-            if set -q _flag_l || test $mime = image/gif || test $(string split -f 1 / $mime) = video
+            if set -q _flag_l || test $mime = image/gif || \
+                test $mime_prefix = video || test $mime_prefix = audio
                 wl-copy -t text/uri-list file://$(pwd)/$input
                 return
             end
