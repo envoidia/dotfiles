d1bf267a6f13a9e398f12f30bfea03cdae79f8bb
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-05 21:09:13 -0500

    fix kill menu notifs

--- desktop/.config/kill_menu.fish
+++ desktop/.config/kill_menu.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
 set -l program $(ps -eo comm= | fsel --dmenu)
-pkill $program
-notify-send "killed $program"
+
+if test -n "$program"
+    pkill $program
+    notify-send "killed $program"
+end
