38b866f45f84cf1ec260370bc795ce86619778bf
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-08 13:37:02 -0500

    rebind vars to hypr-r bc hypr-v is used (and ctrl-super-v is "unintended")

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -18,7 +18,7 @@ complete -c wl-copy -F
 # fzf.fish
 set fzf_fd_opts -iH
 fzf_configure_bindings --directory=ctrl-super-f --git_log=ctrl-super-l --git_status=ctrl-super-s \
-    --history=ctrl-super-h --processes=ctrl-super-p --variables=ctrl-super-v
+    --history=ctrl-super-h --processes=ctrl-super-p --variables=ctrl-super-r
 
 set DOTNET_ROOT /usr/share/dotnet
 set PATH $PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools
