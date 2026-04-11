8de20d9c9537a9ddf94f89089a0c4f5a4f2171ba
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-11 11:49:53 -0500

    fix newline escaping

--- desktop/.config/fish/functions/fish_greeting.fish
+++ desktop/.config/fish/functions/fish_greeting.fish
@@ -22,7 +22,7 @@ function fish_greeting
         "Think I'm at the end of a road traveled far too long" \
         "Love is an incision that cuts deeper than a knife" \
         # Pull the Trigger
-        "TAKE ONE LOOK AT ME, I'LL BE YOUR MEMENTO MORI"
+        "TAKE ONE LOOK AT ME, I'LL BE YOUR MEMENTO MORI" \
         \
         ## PinnochioP
         # I'm glad you're evil too
