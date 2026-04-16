7b7bd04233b2f38443dbcc0e71ddfc7c6eddda20
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-15 19:34:25 -0500

    add lock option to logout menu

--- desktop/.config/logout_menu.fish
+++ desktop/.config/logout_menu.fish
@@ -1,10 +1,12 @@
 #!/usr/bin/env fish
 
-switch $(echo -e "shutdown\nreboot\nlogout" | fsel --dmenu)
+switch $(echo -e "shutdown\nreboot\nlogout\nlock" | fsel --dmenu)
     case shutdown
         shutdown now
     case reboot
         reboot
     case logout
         command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
+    case lock
+        loginctl lock-session
 end
