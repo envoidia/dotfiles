adcc67347fc5c577e27342b320b5353b3f922739
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-17 15:02:53 -0500

    colorize man, update btop theme

--- desktop/.config/fish/config.fish
+++ desktop/.config/fish/config.fish
@@ -22,5 +22,19 @@ set fzf_fd_opts -iH
 fzf_configure_bindings --directory=ctrl-super-f --git_log=ctrl-super-l --git_status=ctrl-super-s \
     --history=ctrl-super-h --processes=ctrl-super-p --variables=ctrl-super-r
 
-set DOTNET_ROOT /usr/share/dotnet
-set PATH $PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools
+# dotnet
+# todo why is it on path 600 billion times
+# set DOTNET_ROOT /usr/share/dotnet
+# set PATH $PATH:/usr/share/dotnet:/home/envoidia/.dotnet/tools
+
+# Colorize manpages
+set -x LESS_TERMCAP_mb $(printf "\e[1;95m")
+set -x LESS_TERMCAP_md $(printf "\e[1;95m")
+set -x LESS_TERMCAP_us $(printf "\e[4;1;94m")
+set -x LESS_TERMCAP_so $(printf "\e[1;38;5;183;48;5;54m")
+set -x LESS_TERMCAP_me $(printf "\e[0m")
+set -x LESS_TERMCAP_se $(printf "\e[0m")
+set -x LESS_TERMCAP_ue $(printf "\e[0m")
+set -x LESS_TERMCAP_mr $(printf "\e[7m")
+set -x LESS_TERMCAP_mh $(printf "\e[2m")
+set -x GROFF_NO_SGR 1
