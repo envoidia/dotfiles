6b8080ef502afeaaade37570c961c07812c72b1a
envoidia <76633436+envoidia@users.noreply.github.com>
2026-04-25 15:43:14 -0500

    rearrange logout menu

--- desktop/.config/logout_menu.fish
+++ desktop/.config/logout_menu.fish
@@ -1,14 +1,14 @@
 #!/usr/bin/env fish
 
-switch $(echo -e "shutdown\nreboot\nlogout\nlock\nsleep" | fsel --dmenu)
+switch $(echo -e "shutdown\nreboot\nlogout\nsleep\nlock" | fsel --dmenu)
     case shutdown
         shutdown now
     case reboot
         reboot
     case logout
         command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
-    case lock
-        loginctl lock-session
     case sleep
         systemctl suspend
+    case lock
+        loginctl lock-session
 end
